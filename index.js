a6adb3795ea1b0aae99e4863397859fae0761928
EPAY-PRESCO\Ian.Fan <ian.fan@presco.ws>
2024-08-20 14:36:41 +0800

    調整部署設定

--- index.js
+++ index.js
@@ -9,25 +9,26 @@ app.use(cors())
 // Mongodb 設定
 var CONNECTION_STRING = 'mongodb+srv://ianFan:ianFan@cluster0.ofmahog.mongodb.net/todo_app'
 var PORT = process.env.PORT || 5038
-var database
+let client
+let database
 
 async function connectToDatabase() {
-  try {
-    const client = new MongoClient(CONNECTION_STRING)
-    await client.connect()
-    database = client.db()
-
-    console.log('MongoDB Connection Successful')
-  } catch (err) {
-    console.error('Failed to connect to MongoDB', error)
+  if (!client || !client.isConnected) {
+    try {
+      const client = new MongoClient(CONNECTION_STRING)
+      await client.connect()
+      database = client.db()
+  
+      console.log('MongoDB Connection Successful')
+    } catch (err) {
+      console.error('Failed to connect to MongoDB', error)
+    }
   }
 }
 
 app.get('/api/todo_app/GetNote', async(request, response) => {
   try {
-    if (!database) {
-      return response.status(500).send('Database connection not initialized')
-    }
+    await connectToDatabase()
 
     const result = await database.collection('todo_app_collection').find({}).toArray()
     response.send(result)
@@ -38,9 +39,7 @@ app.get('/api/todo_app/GetNote', async(request, response) => {
 
 app.post('/api/todo_app/AddNote', multer().none(), async(request,response)=>{
   try {
-    if (!database) {
-      return response.status(500).send('Database connection not initialized')
-    }
+    await connectToDatabase()
 
     const numOfDocs = await database.collection('todo_app_collection').countDocuments()
     await database.collection('todo_app_collection').insertOne({
@@ -62,6 +61,5 @@ app.delete('/api/todo_app/DeleteNote',(request,response)=>{
 })
 
 app.listen(PORT, async () => {
-  await connectToDatabase()
   console.log(`Server is running on port ${PORT}`)
 })
